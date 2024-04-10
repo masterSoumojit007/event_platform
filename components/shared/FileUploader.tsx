ad6c3552c8499d330898663b2c80552a7388b886
Soumojit <soumojitbanerjee22@gmai.com>
2024-04-10 11:14:23 +0530

    updated

--- components/shared/FileUploader.tsx
+++ components/shared/FileUploader.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 import { useCallback, Dispatch, SetStateAction } from "react";
-// import type { FileWithPath } from '@uploadthing/react'
-// import { useDropzone } from '@uploadthing/react/hooks'
 import { FileWithPath, useDropzone } from "react-dropzone";
 import { generateClientDropzoneAccept } from "uploadthing/client";
 
